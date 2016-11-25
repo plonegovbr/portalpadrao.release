9584fcdf5dee7793936328919bc0fbb71530ff2d
idgserpro <idg@serpro.gov.br>
2016-11-25 15:31:44 -0200

    Release 1.1.5.2.
    Adiciona 'versions = versions' para poder corrigir o erro de
    portal.buildout quando utiliza o versions-sem-extends.cfg.

--- versions-sem-extends.sh
+++ versions-sem-extends.sh
@@ -27,9 +27,11 @@ else
             # Garantir que tenho valores únicos e em ordem alfabética
             bin/buildout -c production.cfg annotate | sed '1,/\[versions\]/d' | sed '/^$/,$ d' | sed '/    /d' | sed 's/= / = /' | uniq | sort > versions-sem-extends.cfg
 
-            # Aqui, readiciono o '[versions]' removido acima e uma mensagem
+            # BBB: Aqui, adiciono 'versions = versions' por causa da versão do
+            # buildout utilizada, 1.7.1;
+            # Readiciono o '[versions]' removido acima e uma mensagem
             # indicando que esse arquivo é gerado automaticamente.
-            sed -i '1s/^/\[versions\]\n# NÃO EDITE ESSE ARQUIVO, ele foi gerado dinamicamente pelo script https:\/\/raw.githubusercontent.com\/plonegovbr\/portalpadrao.release\/master\/versions-sem-extends.cfg\n# Utilize esse arquivo caso o seu servidor não possua acesso à internet, uma vez que o versions.cfg tenta acessar vários cfgs externos no extends.\n# Para maiores informações, acesse a seção "Por que versions.cfg e versions-sem-extends.cfg? Qual devo usar?" no README.md em https:\/\/github.com\/plonegovbr\/portalpadrao.release\/blob\/master\/README.md\n/' versions-sem-extends.cfg
+            sed -i '1s/^/\[buildout\]\n# NÃO EDITE ESSE ARQUIVO, ele foi gerado dinamicamente pelo script https:\/\/raw.githubusercontent.com\/plonegovbr\/portalpadrao.release\/master\/versions-sem-extends.cfg\n# Utilize esse arquivo caso o seu servidor não possua acesso à internet, uma vez que o versions.cfg tenta acessar vários cfgs externos no extends.\n# Para maiores informações, acesse a seção "Por que versions.cfg e versions-sem-extends.cfg? Qual devo usar?" no README.md em https:\/\/github.com\/plonegovbr\/portalpadrao.release\/blob\/master\/README.md\n\n# Ver em https:\/\/raw.githubusercontent.com\/plonegovbr\/portalpadrao.release\/master\/versions.cfg a motivação de "versions = versions".\nversions = versions\n\n\[versions\]\n/' versions-sem-extends.cfg
 
             echo "Arquivo versions-sem-extends compilado presente em $PWD/versions-sem-extends.cfg."
         fi
